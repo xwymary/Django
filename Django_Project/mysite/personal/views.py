67de402b34b7d37548afeb4a6f242808a0ffcecf
xwymary <weiyi.xiao@mail.mcgill.ca>
2017-03-02 23:08:20 -0500

    Add files via upload

---
+++ Django_Project/mysite/personal/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content':['If you would like to contact me, please email me','xwymary7@gmail.com']})
+
+
